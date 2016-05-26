bde460e4347335f68652edc3bfa53214e2f21d4c
jokedurnez <joke.durnez@gmail.com>
2016-05-25 23:03:27 -0700

    Compute percent bold change with featquery

--- HCP_group_effect_sizes/EffectSize/EffectSize.py
+++ HCP_group_effect_sizes/EffectSize/EffectSize.py
@@ -6,11 +6,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 import csv
+import shutil
 
 Contrast = sys.argv[1]
 ConDir = sys.argv[2]
-FslDir = os.environ.get('FSLDIR')
 OutDir = sys.argv[3]
+FslDir = os.environ.get('FSLDIR')
 
 Masks={
 "tfMRI_MOTOR":[["C",6],
@@ -32,35 +33,65 @@ VarCopeFile = os.path.join(ConDir,'group.gfeat','cope1.feat','stats','varcope1.n
 VarCope = nib.load(VarCopeFile).get_data()
 
 # read masks
-HarvardOxfordCortFile = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-cort-maxprob-thr25-2mm.nii.gz')
-HarvardOxfordSubFile = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-sub-maxprob-thr25-2mm.nii.gz')
+HarvardOxfordCortFileAll = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-cort-prob-2mm.nii.gz')
+HarvardOxfordSubFileAll = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-sub-prob-2mm.nii.gz')
 HarvardOxford = {
-    "C":nib.load(HarvardOxfordCortFile).get_data(),
-    "S":nib.load(HarvardOxfordSubFile).get_data()
+    "C":nib.load(HarvardOxfordCortFileAll).get_data(),
+    "S":nib.load(HarvardOxfordSubFileAll).get_data()
 }
+HarvardOxfordFile = {
+    "C":HarvardOxfordCortFileAll,
+    "S":HarvardOxfordSubFileAll
+}
+
 
 # Compute Cohens D
 D = Cope/VarCope/np.sqrt(186)
 
 # extract coordinates of masks
-mask_ind = pd.DataFrame()
 for msk,no in Masks[Contrast]:
-    indices = list(np.where(HarvardOxford[msk]==no+1))
-    ind_df = pd.DataFrame(indices).transpose()
-    ind_df.columns=['x','y','z']
-    mask_ind = mask_ind.append(ind_df)
 
-mask_ind = mask_ind.drop_duplicates()
-if Contrast == "tfMRI_LANGUAGE":
-    mask_ind = mask_ind[mask_ind.x<45]
+    print("Contrast : ",Contrast)
+    print("Masktype : ",msk)
+    print("Atlas number : ",no)
+    indd = np.where(HarvardOxford[msk][:,:,:,no]>0)
+    mask_ind = pd.DataFrame()
+    mask_ind['x'] = indd[0]
+    mask_ind['y'] = indd[1]
+    mask_ind['z'] = indd[2]
+    #print(mask_ind)
 
-# export values
-#PERCENTBOLD = np.mean(Cope[mask_ind.x,mask_ind.y,mask_ind.z])/10000
-COHENSD = np.nanmedian(D[mask_ind.x,mask_ind.y,mask_ind.z])
-COHENS25 = np.nanpercentile(D[mask_ind.x,mask_ind.y,mask_ind.z],25)
-COHENS75 = np.nanpercentile(D[mask_ind.x,mask_ind.y,mask_ind.z],75)
-SIZE = len(mask_ind)
+    # export values
+    COHENSD = np.nanmedian(D[mask_ind.x,mask_ind.y,mask_ind.z])
+    COHENS10 = np.nanpercentile(D[mask_ind.x,mask_ind.y,mask_ind.z],10)
+    COHENS90 = np.nanpercentile(D[mask_ind.x,mask_ind.y,mask_ind.z],90)
+    SIZE = len(mask_ind)
 
-with open(os.path.join(OutDir,"es.csv"),'a') as output_file:
-    dict_writer = csv.writer(output_file)
-    dict_writer.writerow([SIZE,COHENS25,COHENSD,COHENS75])
+    # compute percent bold
+
+    tmpquer = ConDir+"group.gfeat/cope1.feat/featquery"
+    featroi_cmd = "fslroi %s %s %s 1" %(HarvardOxfordFile[msk],tmpquer,no)
+    os.popen(featroi_cmd).read()
+    featquery_cmd = "featquery 1 %s/group.gfeat/cope1.feat 2 stats/pe1 stats/cope1 featquery -p -s -w -b %s" %(ConDir,tmpquer)
+    os.popen(featquery_cmd).read()
+
+    featquery_file = ConDir+"group.gfeat/cope1.feat/featquery/report.txt"
+    for line in open(featquery_file):
+        list = line.split(" ")
+
+    # devide by 2 (except motor) because the contrastmatrices are [1 -1]
+    # motor is just an average effect
+    if Contrast == "tfMRI_MOTOR":
+        PERCENT = float(list[6])
+        PERCENT10 = float(list[4])
+        PERCENT90 = float(list[7])
+    else:
+        PERCENT = float(list[6])/2
+        PERCENT10 = float(list[4])/2
+        PERCENT90 = float(list[7])/2
+
+    shutil.rmtree(ConDir+"group.gfeat/cope1.feat/featquery/")
+
+    with open(os.path.join(OutDir,"es.csv"),'a') as output_file:
+        dict_writer = csv.writer(output_file)
+        dict_writer.writerow([Contrast,msk,no,SIZE,COHENS10,COHENSD,COHENS90,PERCENT10,PERCENT,PERCENT90])
--- HCP_group_effect_sizes/group_effect_sizes.sh
+++ HCP_group_effect_sizes/group_effect_sizes.sh
@@ -1,15 +1,26 @@
+#!/bin/bash
+
+#SBATCH --job-name=EffectSizes
+#SBATCH --output=error/out.EffectSizes
+#SBATCH --error=error/err.EffectSizes
+#SBATCH --time=3:00:00
+#SBATCH --nodes=1
+#SBATCH --partition=russpold
+#SBATCH --qos=russpold
+
 # Directory of Connectome in a box
 ConnectomeInABoxDir=$HCPDIR
 # Experiment to be analyzed
-Paradigm=("tfMRI_MOTOR" "tfMRI_WM" "tfMRI_LANGUAGE" "tfMRI_EMOTION" "tfMRI_GAMBLING")
+Paradigm=("tfMRI_MOTOR" "tfMRI_WM" "tfMRI_EMOTION" "tfMRI_GAMBLING")
 # Which contrast from the experiment
-Contrast=(7 11 2 3 6)
+Contrast=(7 11 3 6)
 # Working Directory
-WorkDir=`pwd`
+WorkDir=/home/jdurnez/effect_sizes/HCP_group_effect_sizes/
+
 # File with subjects to be included and disks on which they appear
 SubjectsFile=$WorkDir/SubjectSelection/IDs_all_cons_and_unrelated.txt
 
-for exp in {0..4} ; do
+for exp in {0..3} ; do
 
   ConDir=$WorkDir/GroupAnalyses/${Paradigm[exp]}/
   mkdir $ConDir
