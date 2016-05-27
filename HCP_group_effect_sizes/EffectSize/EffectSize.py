14268de40abf911acbb2409c1b87d2cca41e4264
jokedurnez <joke.durnez@gmail.com>
2016-05-26 18:48:37 -0700

    Smaller masks (fROI + anatomical) + better output

--- HCP_group_effect_sizes/EffectSize/EffectSize.py
+++ HCP_group_effect_sizes/EffectSize/EffectSize.py
@@ -14,17 +14,17 @@ OutDir = sys.argv[3]
 FslDir = os.environ.get('FSLDIR')
 
 Masks={
-"tfMRI_MOTOR":[["C",6],
-                ["C",25],
-                ["S",5],
-                ["S",16]],
-'tfMRI_WM':[["C",3]],
-'tfMRI_LANGUAGE':[["C",6]],
-'tfMRI_EMOTION':[["S",9],
-                ["S",19]],
-'tfMRI_GAMBLING':[["S",10],
-                ["S",20]]
+    "MOTOR":[["Precentral Gyrus",1,os.path.join(OutDir,"CreateMasks","masks","MOTOR_1_CJ.nii.gz")],
+            ["Supplementary motor cortex",2,os.path.join(OutDir,"CreateMasks","masks","MOTOR_2_CJ.nii.gz")],
+            ["Left putamen",3,os.path.join(OutDir,"CreateMasks","masks","MOTOR_3_CJ.nii.gz")],
+            ["Right putamen",4,os.path.join(OutDir,"CreateMasks","masks","MOTOR_4_CJ.nii.gz")]],
+    'WM':[["Middle frontal gyrus",1,os.path.join(OutDir,"CreateMasks","masks","WM_1_CJ.nii.gz")]],
+    'EMOTION':[["Left amygdala",1,os.path.join(OutDir,"CreateMasks","masks","EMOTION_1_CJ.nii.gz")],
+            ["Right amygdala",2,os.path.join(OutDir,"CreateMasks","masks","EMOTION_2_CJ.nii.gz")]],
+    'GAMBLING':[["Left accumbens",1,os.path.join(OutDir,"CreateMasks","masks","GAMBLING_1_CJ.nii.gz")],
+            ["Right accumbens",2,os.path.join(OutDir,"CreateMasks","masks","GAMBLING_2_CJ.nii.gz")]]
 }
+
 
 # read cope + varcope
 CopeFile = os.path.join(ConDir,'group.gfeat','cope1.feat','stats','cope1.nii.gz')
@@ -32,29 +32,14 @@ Cope = nib.load(CopeFile).get_data()
 VarCopeFile = os.path.join(ConDir,'group.gfeat','cope1.feat','stats','varcope1.nii.gz')
 VarCope = nib.load(VarCopeFile).get_data()
 
-# read masks
-HarvardOxfordCortFileAll = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-cort-prob-2mm.nii.gz')
-HarvardOxfordSubFileAll = os.path.join(FslDir,'data','atlases','HarvardOxford','HarvardOxford-sub-prob-2mm.nii.gz')
-HarvardOxford = {
-    "C":nib.load(HarvardOxfordCortFileAll).get_data(),
-    "S":nib.load(HarvardOxfordSubFileAll).get_data()
-}
-HarvardOxfordFile = {
-    "C":HarvardOxfordCortFileAll,
-    "S":HarvardOxfordSubFileAll
-}
-
-
 # Compute Cohens D
 D = Cope/VarCope/np.sqrt(186)
 
 # extract coordinates of masks
-for msk,no in Masks[Contrast]:
+for a in range(len(Masks[Contrast])):
 
-    print("Contrast : ",Contrast)
-    print("Masktype : ",msk)
-    print("Atlas number : ",no)
-    indd = np.where(HarvardOxford[msk][:,:,:,no]>0)
+    mask = Masks[Contrast][a][2]
+    indd = np.where(nib.load(mask).get_data()>0)
     mask_ind = pd.DataFrame()
     mask_ind['x'] = indd[0]
     mask_ind['y'] = indd[1]
@@ -69,10 +54,7 @@ for msk,no in Masks[Contrast]:
 
     # compute percent bold
 
-    tmpquer = ConDir+"group.gfeat/cope1.feat/featquery"
-    featroi_cmd = "fslroi %s %s %s 1" %(HarvardOxfordFile[msk],tmpquer,no)
-    os.popen(featroi_cmd).read()
-    featquery_cmd = "featquery 1 %s/group.gfeat/cope1.feat 2 stats/pe1 stats/cope1 featquery -p -s -w -b %s" %(ConDir,tmpquer)
+    featquery_cmd = "featquery 1 %s/group.gfeat/cope1.feat 2 stats/pe1 stats/cope1 featquery -p -s -w -b %s" %(ConDir,mask)
     os.popen(featquery_cmd).read()
 
     featquery_file = ConDir+"group.gfeat/cope1.feat/featquery/report.txt"
@@ -81,7 +63,7 @@ for msk,no in Masks[Contrast]:
 
     # devide by 2 (except motor) because the contrastmatrices are [1 -1]
     # motor is just an average effect
-    if Contrast == "tfMRI_MOTOR":
+    if Contrast == "MOTOR":
         PERCENT = float(list[6])
         PERCENT10 = float(list[4])
         PERCENT90 = float(list[7])
@@ -94,4 +76,4 @@ for msk,no in Masks[Contrast]:
 
     with open(os.path.join(OutDir,"es.csv"),'a') as output_file:
         dict_writer = csv.writer(output_file)
-        dict_writer.writerow([Contrast,msk,no,SIZE,COHENS10,COHENSD,COHENS90,PERCENT10,PERCENT,PERCENT90])
+        dict_writer.writerow([Contrast,Masks[Contrast][a][0],SIZE,COHENS10,COHENSD,COHENS90,PERCENT10,PERCENT,PERCENT90])
--- HCP_group_effect_sizes/group_analyses.sh
+++ HCP_group_effect_sizes/group_analyses.sh
@@ -11,7 +11,7 @@
 # Directory of Connectome in a box
 ConnectomeInABoxDir=$HCPDIR
 # Experiment to be analyzed
-Paradigm=("MOTOR" "WM" "EMOTION" "GAMBLING")
+Paradigm=("tfMRI_MOTOR" "tfMRI_WM" "tfMRI_EMOTION" "tfMRI_GAMBLING")
 # Which contrast from the experiment
 Contrast=(7 11 3 6)
 # Working Directory
@@ -22,9 +22,13 @@ SubjectsFile=$WorkDir/SubjectSelection/IDs_all_cons_and_unrelated.txt
 
 for exp in {0..3} ; do
 
-  ConDir=$WorkDir/GroupAnalyses/tfMRI_${Paradigm[exp]}/
+  ConDir=$WorkDir/GroupAnalyses/${Paradigm[exp]}/
+  mkdir $ConDir
 
-  # Extract Effectsizes
-  python $WorkDir/EffectSize/EffectSize.py ${Paradigm[exp]} $ConDir $WorkDir/
+  # Make design.fsf and design.grp
+  python $WorkDir/GroupAnalyses/make_design.py $SubjectsFile $ConnectomeInABoxDir ${Paradigm[exp]} ${Contrast[exp]} $ConDir
+
+  # Analyze group stats
+  feat $ConDir/design
 
 done
