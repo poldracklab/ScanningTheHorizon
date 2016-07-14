1ad46b4261d632551bfd485e1496cd42bc2d7a73
poldrack <poldrack@gmail.com>
2016-07-14 10:43:13 -0700

    started working on separating group and single studies

--- SampleSize/get_sampsize_combined.py
+++ SampleSize/get_sampsize_combined.py
@@ -6,10 +6,10 @@ Load the spreadsheet of sample sizes manually annotated by Joe Wexler
 based on the neurosynth automated estimates
 
 """
-
+import os
 import numpy,pandas
 from get_pmid_data_from_pmid import get_pmid_data_from_pmid
-
+from filter_fMRI_terms import filter_fMRI_terms
 
 def load_worksheet(fname='estimated_n_format_real.csv',verbose=True):
     """load data and clean up"""
@@ -33,4 +33,32 @@ def get_pubmed_records(d):
             problem_pmids.append(pmid)
     return pmidrecs,problem_pmids
 
-pmidrecs=get_pubmed_records(wsdata)
+if os.path.exists('fullset_pmid_records'):
+    pmidrecs=pickle.load(open('fullset_pmid_records','rb'))
+else:
+    pmidrecs=get_pubmed_records(wsdata)
+    pmidrecs_fmri=filter_fMRI_terms(pmidrecs)
+    pickle.dump(pmidrecs,open('fullset_pmid_records','wb'))
+
+# get single-study and group sizes
+
+def get_study_and_group_sizes(wsdata):
+    """ split records into single-group studies ('study') and
+    multi-group studies ('group')
+    """
+    
+    study_sizes={}
+    group_sizes={}
+
+    for pmid,kind,n_actual in zip(wsdata.PMID,wsdata.kind,wsdata.n_actual):
+        if kind=='study':
+            # make sure there are no duplicates
+            assert pmid not in study_sizes
+            study_sizes[pmid]=n_actual
+        elif kind=='group':
+            if not pmid in study_sizes:
+                study_sizes[pmid]=[]
+            study_sizes[pmid].append(n_actual)
+    return study_sizes,group_sizes
+
+study_sizes,group_sizes=get_study_and_group_sizes(wsdata)
