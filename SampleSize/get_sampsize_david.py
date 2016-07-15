08d2ab82dc0c200a8ce8e4889a98d9831c468303
poldrack <poldrack@gmail.com>
2016-07-15 09:18:59 -0700

    renaming files for clarity, removed older neurosynth code from get_sampsize_david

--- SampleSize/get_sampsize_david.py
+++ SampleSize/get_sampsize_david.py
@@ -1,6 +1,5 @@
 """
 analysis of sample sizes from David et al. PLOS one data
-and neurosynth automatic estimates
 
 Copyright Russell Poldrack, 2016
 """
@@ -12,6 +11,8 @@ import requests
 from Bio import Entrez
 import pickle
 import time
+from filter_fMRI_terms import filter_fMRI_terms
+from get_pmid_data_from_pmid import get_pmid_data_from_pmid
 
 # should change this to your own email address
 Entrez.email='slacker@harvard.edu'
@@ -180,10 +181,9 @@ def get_pmids_for_dois(doi_records,verbose=False):
                 print(record['IdList'])
             bad_cvt.append(id)
 
-        if 0:
             if verbose:
                 print('problem searching for %s'%id)
-            bad_cvt.append(id)
+
     pickle.dump(doi_pmid_cvt,open( 'doi_pmid_cvt.pkl','wb'))
     pickle.dump(doi_pmids,open( 'doi_pmids.pkl','wb'))
     return doi_pmids,doi_pmid_cvt,bad_cvt
@@ -194,34 +194,6 @@ try:
 except FileNotFoundError:
     doi_pmids,doi_pmid_cvt,bad_cvt=get_pmids_for_dois(doi_records,verbose=True)
 
-
-
-# get paper information from PMID using Entrez tools
-def get_pmid_data_from_pmid(pmid, delay=0.34,verbose=False):
-    """ use entrez API tool to get full record for a PMID"""
-
-    pmid=int(pmid)
-    handle = Entrez.efetch("pubmed", id="%s"%pmid, retmode="xml")
-    time.sleep(delay)
-    records=Entrez.parse(handle)
-    rec=[i for i in records]
-    if len(rec)>1:
-        print('Warning: record of length >1',pmid,rec)
-    if verbose:
-        print(rec)
-    data={}
-    data['Journal']=rec[0]['MedlineCitation']['Article']['Journal']['ISOAbbreviation']
-    data['Title']=rec[0]['MedlineCitation']['Article']['ArticleTitle']
-    data['Abstract']=rec[0]['MedlineCitation']['Article']['Abstract']['AbstractText']
-    data['PubDate']=rec[0]['MedlineCitation']['Article']['Journal']['JournalIssue']['PubDate']
-    data['DateCreated']=rec[0]['MedlineCitation']['DateCreated']['Year']
-    data['MeshTerms']=[]
-    if len(rec[0]['MedlineCitation']['MeshHeadingList'])>0:
-        for i in range(len(rec[0]['MedlineCitation']['MeshHeadingList'])):
-            data['MeshTerms'].append(str(rec[0]['MedlineCitation']['MeshHeadingList'][i]['DescriptorName']))
-    if verbose:
-        print(pmid,data)
-    return data,rec
 
 
 pmids=david_pmids + doi_pmids
@@ -249,25 +221,6 @@ else:
 # check for specific mention of fMRI-related terms
 # to filter out non-fMRI papers
 
-def filter_fMRI_terms(pmids,fmri_terms=['fMRI','functional MRI',
-                        'functional magnetic resonance']):
-    """ return pmids that include fMRI-related terms
-    in MESH keywords or abstract or title"""
-
-    good_pmids={}
-    for pmid in pmids.keys():
-        goodkey=0
-        if 'MeshTerms' in pmids[pmid].keys():
-            if 'Magnetic Resonance Imaging' in pmids[pmid]['MeshTerms']:
-                goodkey=1
-        for t in fmri_terms:
-            if pmids[pmid]['Abstract'][0].find(t)>-1:
-                goodkey=1
-            if pmids[pmid]['Title'].find(t)>-1:
-                goodkey=1
-        if goodkey:
-            good_pmids[pmid]=pmids[pmid]
-    return good_pmids
 
 print('original David list: %d items'%len(pmid_data))
 pmid_data=filter_fMRI_terms(pmid_data)
@@ -332,91 +285,3 @@ for k in pubyear.keys():
     except:
         pass
 numpy.savetxt('david_sampsizedata.txt',alldata)
-
-
-
-# load neurosynth data
-
-def load_neurosynth_data(infile='estimated_n.txt'):
-    f=open(infile)
-    header=f.readline()
-    lines=[i.strip().split('\t') for i in f.readlines()]
-    return lines
-
-lines=load_neurosynth_data()
-
-# get all N estimates for each pmid
-# previously we had just taken the first but that was problematic
-
-
-def get_neurosynth_n_estimates(lines,verbose=False):
-    """extract pmid and estimated n from lines of text file"""
-    ns_n_estimates={}
-    for l in lines:
-        if verbose:
-            print(l[:2])
-        pmid=int(l[0])
-        if not pmid in ns_n_estimates.keys():
-            ns_n_estimates[pmid]=[]
-        ns_n_estimates[pmid].append(int(l[1]))
-    return ns_n_estimates
-
-
-
-
-
-def get_neurosynth_data_from_pmids(lines,verbose=False):
-    ns_n_estimates=get_neurosynth_n_estimates(lines)
-
-    tal_data={}
-    tal_problem_pmid=[]
-    for l in lines:
-        pmid=int(l[0])
-        # only include abstracts with a single N estimate
-        # there does not appear to be a good strategy to accurately
-        # assess sample size when there are multiple N's reported
-
-        if len(ns_n_estimates[pmid])>1:
-            continue
-        try:
-            assert pmid in ns_n_estimates
-        except AssertionError:
-            if verbose:
-                print('missing n estimate:',pmid)
-            continue
-
-        if not pmid in tal_data:
-            try:
-                tal_data[pmid],_=get_pmid_data_from_pmid(pmid)
-            except KeyError:
-                continue
-        else:
-            raise Exception('duplicate pmids - check this out')
-
-        tal_data[pmid]['nest']=ns_n_estimates[pmid]
-
-        if verbose:
-            print(tal_data[pmid],l[1:])
-
-        tal_data[pmid]['pubdate']=get_pubdate(tal_data[pmid])
-
-    pickle.dump(tal_data,open('tal_data.pkl','wb'))
-    return tal_data
-
-if os.path.exists('tal_data.pkl'):
-    tal_data=pickle.load(open('tal_data.pkl','rb'))
-else:
-    tal_data=get_neurosynth_data_from_pmids(lines)
-
-
-print('original neurosynth list: %d items'%len(tal_data))
-tal_data=filter_fMRI_terms(tal_data)
-print('filtered neurosynth list: %d items'%len(tal_data))
-
-tal_data_mtx=[]
-for pmid in tal_data.keys():
-    if 'nest' in tal_data[pmid]:
-        tal_data_mtx.append([int(tal_data[pmid]['pubdate']),tal_data[pmid]['nest'][0]])
-tal_data_mtx=numpy.array(tal_data_mtx)
-
-numpy.savetxt('neurosynth_sampsizedata.txt',tal_data_mtx)
